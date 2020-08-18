8d2a7fba7129049cfb6be0dbf30887f6985835df
Sheon Xu <azsxqu@gmail.com>
2020-08-18 18:30:13 -0400

    created helloworld and loop

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+Author:       Sheon Xu
+Date Created: 18.8.2020
+Last Updated: 18.8.2020
+Title:        HelloWorld
+*/
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.print("Hello World");
+  }
+
+}
---
+++ Loop.java
@@ -0,0 +1,20 @@
+/*
+Author:       Sheon Xu
+Date Created: 18.8.2020
+Last Updated: 18.8.2020
+Title:        Loop
+*/
+
+public class Loop {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i + 1);
+    }
+    int cntr = 0;
+    while (cntr < 10) {
+      cntr++;
+      System.out.println(cntr);
+    }
+  }
+}
